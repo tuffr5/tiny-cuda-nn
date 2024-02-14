fix overflow bugs

compression/bitstream/decode.py
@@ -9,7 +9,14 @@ from torch import Tensor
 from bitstream.header import read_frame_header
 from bitstream.range_coder import RangeCoder
 from models.network import QuantizableNetworkWithInputEncoding
-from utils.misc import generate_input_grid, MAX_AC_MAX_VAL
+from utils.misc import (
+    generate_input_grid, 
+    MAX_AC_MAX_VAL, 
+    MAX_HASH_LEVEL_BYTES,
+    MAX_NN_BYTES, 
+    MAX_PARAM_SHAPE,
+    get_num_levels
+    )
 
 
 @torch.no_grad()
@@ -55,7 +62,11 @@ def decode_frame(
     # =========================== Decode the encoding and network ============ #
     params = []
     range_coder = RangeCoder(AC_MAX_VAL = MAX_AC_MAX_VAL)
-    for shape_i, mu_i, scale_i, n_bytes_i in header_info.get("shape_mu_scale_and_n_bytes"):
+    for i, (shape_i, mu_i, scale_i, n_bytes_i) in enumerate(header_info.get("shape_mu_scale_and_n_bytes")):
+        # hack back
+        shape_i = MAX_PARAM_SHAPE if shape_i == 0 else shape_i
+        limitation = MAX_HASH_LEVEL_BYTES if i < get_num_levels(header_info.get('encoding_configs')) else MAX_NN_BYTES
+        n_bytes_i = limitation if n_bytes_i == 0 else n_bytes_i
         range_coder.load_bitstream(bitstream[:n_bytes_i])
         model_param_quant_i = range_coder.decode(torch.zeros(shape_i), torch.ones(shape_i))
         model_param_quant_i = (model_param_quant_i - mu_i) * scale_i 

compression/bitstream/range_coder.py
@@ -19,11 +19,11 @@ class RangeCoder:
         # Actual q_step is 1 / Q_PROBA
         self.Q_PROBA = Q_PROBA
 
-        # Data are in [-AC_MAX_VAL -1, AC_MAX_VAL]
+        # Data are in [-AC_MAX_VAL, AC_MAX_VAL + 1]
         self.AC_MAX_VAL = AC_MAX_VAL
 
-        self.alphabet = np.arange(-self.AC_MAX_VAL - 1, self.AC_MAX_VAL)
-        self.model_family = constriction.stream.model.QuantizedLaplace(-self.AC_MAX_VAL - 1, self.AC_MAX_VAL)
+        self.alphabet = np.arange(-self.AC_MAX_VAL, self.AC_MAX_VAL + 1)
+        self.model_family = constriction.stream.model.QuantizedLaplace(-self.AC_MAX_VAL, self.AC_MAX_VAL + 1)
 
     def quantize_proba_parameters(self, x: Tensor) -> Tensor:
         """Apply a quantization to the input x to reduce floating point

compression/example.py
@@ -52,7 +52,7 @@ class Image(torch.nn.Module):
         
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Image Encoding.")
+    parser = argparse.ArgumentParser(description="Image compression example.")
 
     parser.add_argument("image_str", nargs="?", default="data/images/kodim04.png", help="Image to match")
     parser.add_argument("config", nargs="?", default="data/config_hash.json", help="JSON config for tiny-cuda-nn",)

compression/models/quantizable.py
@@ -35,9 +35,11 @@ class QuantizableModule(nn.Module):
         assert self.param_partitions is not None, 'You must initialize the parameter partitions before initializing the quantization parameters.'
         if params is not None:
             params = self.fragment_param(params)
-            self._scale = nn.Parameter(torch.tensor([2 * p.abs().max() / MAX_AC_MAX_VAL for p in params]), requires_grad=True)
-            self._mu = torch.tensor([-p.min() for p in params]) / torch.max(self._scale.detach(), torch.tensor(1e-5))
-            self._mu = torch.max(self._mu.round_(), torch.tensor(0.))
+            p_max = torch.tensor([p.abs().max() for p in params])
+            p_min = torch.tensor([-p.abs().max() if p.min() < 0 else 0 for p in params])
+            self._scale = nn.Parameter((p_max - p_min) / (MAX_AC_MAX_VAL + 1), requires_grad=True)
+            self._mu = -p_min / torch.max(self._scale.detach(), torch.tensor(1e-5))
+            self._mu = self._mu.round_()
         else:
             self._mu = torch.zeros(len(self.param_partitions))
             self._scale = nn.Parameter(torch.rand(len(self.param_partitions), 1), requires_grad=True)
@@ -95,7 +97,7 @@ class QuantizableModule(nn.Module):
             # print(f"[Before]mu: {torch.amax(mu)}, scale: {torch.amax(scale)}")
             # print(f"[Before]mu: {torch.amin(mu)}, scale: {torch.amin(scale)}")
             # scale = scale * 1.0 / math.sqrt(p.numel() * MAX_AC_MAX_VAL)
-            sent_param = (round_ste(p / scale) + mu).clamp(-MAX_AC_MAX_VAL-1, MAX_AC_MAX_VAL)
+            sent_param = (round_ste(p / scale) + mu).clamp(-MAX_AC_MAX_VAL, MAX_AC_MAX_VAL + 1)
             sent_param = (sent_param - mu) * scale
 
             # print(f"[After]mu: {torch.amax(mu)}, scale: {torch.amax(scale)}, sent_param: {torch.amax(sent_param)}")